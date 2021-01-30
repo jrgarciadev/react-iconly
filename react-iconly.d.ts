6ec6e7b408dcfbd0875777e1df0e4033d2fa3753
Rodolfo Silva <contato@rodolfosilva.com>
2021-01-30 11:49:17 -0300

    feat: add typescript declarations

---
+++ react-iconly.d.ts
@@ -0,0 +1,116 @@
+import React, { CSSProperties } from 'react'
+
+type Color = 'currentColor' | string
+
+export interface IconProps {
+  label?: string
+  filled?: boolean
+  primaryColor?: Color
+  secondaryColor?: Color
+  size?: number | 'small' | 'medium' | 'large' | 'xlarge'
+  set?: 'light' | 'bold' | 'two-tone' | 'bulk' | 'broken'
+  stroke?: 'light' | 'regular' | 'bold'
+  style?: CSSProperties
+}
+
+export declare const Search: React.FC<IconProps>
+export declare const Folder: React.FC<IconProps>
+export declare const Wallet: React.FC<IconProps>
+export declare const Graph: React.FC<IconProps>
+export declare const Bookmark: React.FC<IconProps>
+export declare const Category: React.FC<IconProps>
+export declare const Home: React.FC<IconProps>
+export declare const Notification: React.FC<IconProps>
+export declare const Chat: React.FC<IconProps>
+export declare const Heart: React.FC<IconProps>
+export declare const Heart2: React.FC<IconProps>
+export declare const Paper: React.FC<IconProps>
+export declare const PaperPlus: React.FC<IconProps>
+export declare const PaperNegative: React.FC<IconProps>
+export declare const PaperFail: React.FC<IconProps>
+export declare const PaperDownload: React.FC<IconProps>
+export declare const PaperUpload: React.FC<IconProps>
+export declare const Send: React.FC<IconProps>
+export declare const Password: React.FC<IconProps>
+export declare const Swap: React.FC<IconProps>
+export declare const Work: React.FC<IconProps>
+export declare const Scan: React.FC<IconProps>
+export declare const Activity: React.FC<IconProps>
+export declare const Calendar: React.FC<IconProps>
+export declare const Message: React.FC<IconProps>
+export declare const Video: React.FC<IconProps>
+export declare const Plus: React.FC<IconProps>
+export declare const Chart: React.FC<IconProps>
+export declare const Game: React.FC<IconProps>
+export declare const Bag: React.FC<IconProps>
+export declare const Bag2: React.FC<IconProps>
+export declare const TicketStar: React.FC<IconProps>
+export declare const MoreCircle: React.FC<IconProps>
+export declare const MoreSquare: React.FC<IconProps>
+export declare const Discount: React.FC<IconProps>
+export declare const Buy: React.FC<IconProps>
+export declare const InfoCircle: React.FC<IconProps>
+export declare const InfoSquare: React.FC<IconProps>
+export declare const Danger: React.FC<IconProps>
+export declare const CloseSquare: React.FC<IconProps>
+export declare const TickSquare: React.FC<IconProps>
+export declare const Discovery: React.FC<IconProps>
+export declare const Location: React.FC<IconProps>
+export declare const Document: React.FC<IconProps>
+export declare const Setting: React.FC<IconProps>
+export declare const TimeSquare: React.FC<IconProps>
+export declare const TimeCircle: React.FC<IconProps>
+export declare const VolumeUp: React.FC<IconProps>
+export declare const VolumeDown: React.FC<IconProps>
+export declare const VolumeOff: React.FC<IconProps>
+export declare const Star: React.FC<IconProps>
+export declare const Ticket: React.FC<IconProps>
+export declare const Camera: React.FC<IconProps>
+export declare const User: React.FC<IconProps>
+export declare const AddUser: React.FC<IconProps>
+export declare const TwoUsers: React.FC<IconProps>
+export declare const People: React.FC<IconProps>
+export declare const Login: React.FC<IconProps>
+export declare const Logout: React.FC<IconProps>
+export declare const Download: React.FC<IconProps>
+export declare const Upload: React.FC<IconProps>
+export declare const Voice: React.FC<IconProps>
+export declare const Voice2: React.FC<IconProps>
+export declare const Delete: React.FC<IconProps>
+export declare const Edit: React.FC<IconProps>
+export declare const EditSquare: React.FC<IconProps>
+export declare const Play: React.FC<IconProps>
+export declare const ShieldDone: React.FC<IconProps>
+export declare const ShieldFail: React.FC<IconProps>
+export declare const Show: React.FC<IconProps>
+export declare const Hide: React.FC<IconProps>
+export declare const Filter: React.FC<IconProps>
+export declare const Filter2: React.FC<IconProps>
+export declare const Image: React.FC<IconProps>
+export declare const Image2: React.FC<IconProps>
+export declare const Call: React.FC<IconProps>
+export declare const Calling: React.FC<IconProps>
+export declare const CallMissed: React.FC<IconProps>
+export declare const CallSilent: React.FC<IconProps>
+export declare const Lock: React.FC<IconProps>
+export declare const Unlock: React.FC<IconProps>
+export declare const ChevronDownCircle: React.FC<IconProps>
+export declare const ChevronRightCircle: React.FC<IconProps>
+export declare const ChevronUpCircle: React.FC<IconProps>
+export declare const ChevronLeftCircle: React.FC<IconProps>
+export declare const ChevronDown: React.FC<IconProps>
+export declare const ChevronRight: React.FC<IconProps>
+export declare const ChevronUp: React.FC<IconProps>
+export declare const ChevronLeft: React.FC<IconProps>
+export declare const ArrowDownSquare: React.FC<IconProps>
+export declare const ArrowRightSquare: React.FC<IconProps>
+export declare const ArrowUpSquare: React.FC<IconProps>
+export declare const ArrowLeftSquare: React.FC<IconProps>
+export declare const ArrowDown: React.FC<IconProps>
+export declare const ArrowRight: React.FC<IconProps>
+export declare const ArrowUp: React.FC<IconProps>
+export declare const ArrowLeft: React.FC<IconProps>
+export declare const CaretRight: React.FC<IconProps>
+export declare const CaretDown: React.FC<IconProps>
+export declare const CaretUp: React.FC<IconProps>
+export declare const CaretLeft: React.FC<IconProps>
