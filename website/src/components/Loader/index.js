28940a4b5fe66d1d0fa00ff12936a4510cbaf83e
Taylor Clay <8163665+TaylorClay@users.noreply.github.com>
2022-03-02 12:08:13 -0600

    Fix https://github.com/jrgarciadev/react-iconly/issues/23

--- website/src/components/Loader/index.js
+++ website/src/components/Loader/index.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
 import { connectStateResults } from 'react-instantsearch-dom'
 import { StyledLoader } from './styles'
 
-const Loader = connectStateResults(({ isSearchStalled }) =>
-  isSearchStalled ? <StyledLoader /> : null
-)
+const SPINNER_DELAY = 200 // ms
+
+const Loader = connectStateResults(({ isSearchStalled }) => {
+  const [canShowSpinner, setCanShowSpinner] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCanShowSpinner(true);
+    }, SPINNER_DELAY)
+  }, [canShowSpinner, setCanShowSpinner])
+
+  return canShowSpinner && isSearchStalled ? <StyledLoader /> : null
+});
 
 export default Loader
