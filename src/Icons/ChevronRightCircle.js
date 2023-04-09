9a4c564781e01f3de2a22b8774d4a89c6a4dbcd3
Ion Leu <ion.leu@gmail.com>
2023-04-09 11:45:19 -0400

    bugfix(babel): use @babel/eslint-parser and @babel/preset-react instead of depricated one that fixes failing tests

--- src/Icons/ChevronRightCircle.js
+++ src/Icons/ChevronRightCircle.js
@@ -10,7 +10,7 @@ const ChevronRightCircle = ({
   set
 }) => {
   const Bold = () => (
-    <g i transform='translate(2 2)'>
+    <g transform='translate(2 2)'>
       <path
         d='M10,20l-.28,0A10.011,10.011,0,1,1,10,20ZM8.56,5.78a.745.745,0,0,0-.53,1.28L10.98,10,8.03,12.94A.746.746,0,0,0,8.02,14a.752.752,0,0,0,.534.225A.727.727,0,0,0,9.08,14l3.49-3.47a.75.75,0,0,0,0-1.06L9.08,6A.7.7,0,0,0,8.56,5.78Z'
         transform='translate(0 0)'
@@ -19,7 +19,7 @@ const ChevronRightCircle = ({
     </g>
   )
   const Bulk = () => (
-    <g i transform='translate(2 22) rotate(-90)'>
+    <g transform='translate(2 22) rotate(-90)'>
       <path
         d='M20,10A10,10,0,1,1,10,0,10.011,10.011,0,0,1,20,10'
         transform='translate(0 0)'
@@ -34,7 +34,7 @@ const ChevronRightCircle = ({
     </g>
   )
   const Light = () => (
-    <g i transform='translate(2 22) rotate(-90)'>
+    <g transform='translate(2 22) rotate(-90)'>
       <path
         d='M0,9.25A9.25,9.25,0,1,0,9.25,0,9.25,9.25,0,0,0,0,9.25Z'
         transform='translate(0.75 0.75)'
@@ -59,7 +59,7 @@ const ChevronRightCircle = ({
   )
 
   const Broken = () => (
-    <g i transform='translate(2 2)'>
+    <g transform='translate(2 2)'>
       <path
         d='M0,10A9.94,9.94,0,0,1,3.9,2.073.727.727,0,0,1,4.79,3.226,8.546,8.546,0,1,0,10,1.454a8.645,8.645,0,0,0-1.3.1A.727.727,0,0,1,8.481.114,10,10,0,1,1,0,10Zm9.468,2.573L6,9.087A.75.75,0,1,1,7.06,8.028L10,10.981l2.94-2.953A.75.75,0,0,1,14,9.087L10.53,12.573a.747.747,0,0,1-1.062,0Z'
         transform='translate(0 20) rotate(-90)'
@@ -69,7 +69,7 @@ const ChevronRightCircle = ({
   )
 
   const TwoTone = () => (
-    <g i transform='translate(2 22) rotate(-90)'>
+    <g transform='translate(2 22) rotate(-90)'>
       <path
         d='M0,9.25A9.25,9.25,0,1,0,9.25,0,9.25,9.25,0,0,0,0,9.25Z'
         transform='translate(0.75 0.75)'
